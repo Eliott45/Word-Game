6cfea5454e2d0944f28fd5d12a8421c405ee85af
Eliott <eliott_nu@mail.ru>
2021-03-08 12:42:08 +0900

    Creating a level using a class WordLevel

--- Word Game/Assets/__Scripts/WordGame.cs
+++ Word Game/Assets/__Scripts/WordGame.cs
@@ -18,6 +18,7 @@ public class WordGame : MonoBehaviour
 
     [Header("Set Dynamically")]
     public GameMode mode = GameMode.preGame;
+    public WordLevel currLevel;
 
     private void Awake()
     {
@@ -33,5 +34,72 @@ public class WordGame : MonoBehaviour
     public void WordListParseComplete()
     {
         mode = GameMode.makeLevel;
+
+        // Создать уровень и сохранить в currLevel текущий WordLevel
+        currLevel = MakeWordLevel();
+    }
+
+    public WordLevel MakeWordLevel(int levelNum = -1)
+    {
+        WordLevel level = new WordLevel();
+        if(levelNum == -1)
+        {
+            // Выбрать случайный уровень
+            level.longWordIndex = Random.Range(0, WordList.LONG_WORD_COUNT);
+        } else
+        {
+
+        }
+        level.levelNum = levelNum;
+        level.word = WordList.GET_LONG_WORD(level.longWordIndex);
+        level.charDict = WordLevel.MakeCharDict(level.word);
+
+        StartCoroutine(FindSubWordsCoroutine(level));
+        return level;
+    }
+
+    /// <summary>
+    /// Отыскивает слова, которые можно составить на этом уровне
+    /// </summary>
+    public IEnumerator FindSubWordsCoroutine(WordLevel level)
+    {
+        level.subWords = new List<string>();
+        string str;
+
+        List<string> words = WordList.GET_WORDS();
+
+        // Выполнить обход всех слов в WordList
+        for (int i = 0; i < WordList.WORD_COUNT; i++)
+        {
+            str = words[i];
+            // Проверить, можно ли его составить из символов в level.charDict
+            if(WordLevel.CheckWordInLevel(str, level)) { 
+                level.subWords.Add(str);
+            }
+            // Приостановиться после анализа заданного числа слов в этом кадре
+            if(i%WordList.NUM_TO_PARSE_BEFORE_YIELD == 0)
+            {
+                yield return null;
+            }
+        }
+
+        level.subWords.Sort();
+        level.subWords = SortWordByLength(level.subWords).ToList();
+
+        SubWordSearchComplete();
+    }
+
+    /// <summary>
+    /// Использует LINQ для сортировки массива и возвращает его копию
+    /// </summary>
+    public static IEnumerable<string> SortWordByLength(IEnumerable<string> ws)
+    {
+        ws = ws.OrderBy(s => s.Length);
+        return ws;
+    }
+
+    public void SubWordSearchComplete()
+    {
+        mode = GameMode.levelPrep;
     }
 }

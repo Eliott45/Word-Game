10b2349f9bb47f3da77d63431fdbc9316ca3749f
Eliott <eliott_nu@mail.ru>
2021-03-08 16:06:34 +0900

    Method Word.GameLayout

--- Word Game/Assets/__Scripts/WordGame.cs
+++ Word Game/Assets/__Scripts/WordGame.cs
@@ -16,13 +16,24 @@ public class WordGame : MonoBehaviour
 {
     static public WordGame S; // Одиночка
 
+    [Header("Set in Inspector")]
+    public GameObject prefabLetter;
+    public Rect wordArea = new Rect(-24, 19, 48, 28);
+    public float letterSize = 1.5f;
+    public bool showAllWyrds = true; 
+
     [Header("Set Dynamically")]
     public GameMode mode = GameMode.preGame;
     public WordLevel currLevel;
+    public List<Wyrd> wyrds;
+
+    private Transform letterAnchor, bigLetterAnchor;
 
     private void Awake()
     {
         S = this;
+        letterAnchor = new GameObject("LetterAnchor").transform;
+        bigLetterAnchor = new GameObject("BigLetterAnchor").transform;
     }
 
     private void Start()
@@ -46,12 +57,15 @@ public class WordGame : MonoBehaviour
         {
             // Выбрать случайный уровень
             level.longWordIndex = Random.Range(0, WordList.LONG_WORD_COUNT);
+            Debug.Log(level.longWordIndex);
         } else
         {
 
         }
         level.levelNum = levelNum;
-        level.word = WordList.GET_LONG_WORD(level.longWordIndex);
+        Debug.Log("1");
+        level.word = WordList.GET_LONG_WORD(level.longWordIndex); //
+        Debug.Log("2");
         level.charDict = WordLevel.MakeCharDict(level.word);
 
         StartCoroutine(FindSubWordsCoroutine(level));
@@ -84,7 +98,7 @@ public class WordGame : MonoBehaviour
         }
 
         level.subWords.Sort();
-        level.subWords = SortWordByLength(level.subWords).ToList();
+        level.subWords = SortWordsByLength(level.subWords).ToList();
 
         SubWordSearchComplete();
     }
@@ -92,7 +106,7 @@ public class WordGame : MonoBehaviour
     /// <summary>
     /// Использует LINQ для сортировки массива и возвращает его копию
     /// </summary>
-    public static IEnumerable<string> SortWordByLength(IEnumerable<string> ws)
+    public static IEnumerable<string> SortWordsByLength(IEnumerable<string> ws)
     {
         ws = ws.OrderBy(s => s.Length);
         return ws;
@@ -101,5 +115,67 @@ public class WordGame : MonoBehaviour
     public void SubWordSearchComplete()
     {
         mode = GameMode.levelPrep;
+        Layout();
+    }
+
+    void Layout()
+    {
+        // Поместить на экран плитки с буквами каждого возможного слов текущего
+        wyrds = new List<Wyrd>();
+
+        // Объявть локальные переменные, которые будут использоваться методом
+        GameObject go;
+        Letter lett;
+        string word;
+        Vector3 pos;
+        float left = 0;
+        float columnWidth = 3;
+        char c;
+        Color col;
+        Wyrd wyrd;
+
+        // Определить, сколько рядов плиток уместится на экране
+        int numRows = Mathf.RoundToInt(wordArea.height / letterSize);
+
+        // Создать экземпляр Wyrd для каждого слова в level.subWords
+        for (int i = 0; i < currLevel.subWords.Count; i++)
+        {
+            wyrd = new Wyrd();
+            word = currLevel.subWords[i];
+
+            // Если слово длинне, чем columnWidth, развернуть его
+            columnWidth = Mathf.Max(columnWidth, word.Length);
+
+            // Создать экземпляр PrefabLetter для каждой буквы в слове
+            for (int j = 0; j < word.Length; j++)
+            {
+                c = word[j]; // Получить j-й символ слова
+                go = Instantiate<GameObject>(prefabLetter);
+                go.transform.SetParent(letterAnchor);
+                lett = go.GetComponent<Letter>();
+                lett.c = c; // назначить букву плитке 
+
+                // Установить координаты плитки Letter
+                pos = new Vector3(wordArea.x + left + j * letterSize, wordArea.y, 0);
+
+                // Выстроить плитку по вертикали
+                pos.y -= (i % numRows) * letterSize;
+
+                lett.pos = pos;
+
+                go.transform.localScale = Vector3.one * letterSize;
+                wyrd.Add(lett);
+            }
+
+            if (showAllWyrds) wyrd.visible = true;
+
+            wyrds.Add(wyrd);
+
+            // Если достигнут последний ряд в столбце, начать новый столбей
+            if(i % numRows == numRows-1)
+            {
+                left += (columnWidth + 0.5f) * letterSize;
+            }
+        }
     }
 }

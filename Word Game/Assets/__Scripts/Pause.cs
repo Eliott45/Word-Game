5396aa6c5f685c408f2d0edb8909d8d6c4b4e52d
Eliott <eliott_nu@mail.ru>
2021-03-20 13:30:12 +0900

    Pause and restart level

---
+++ Word Game/Assets/__Scripts/Pause.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public GameObject UIPause;
+
+    [Header("Set Dynamically")]
+    public GameObject music;
+    public AudioSource musicVol;
+
+    private bool see = false;
+    private float volume;
+
+    private void Start()
+    {
+        music = GameObject.Find("Music");
+        musicVol = music.GetComponent<AudioSource>();
+        volume = musicVol.volume;
+    }
+
+    private void Update()
+    {
+        musicVol.volume = volume;
+        if (Input.GetKeyDown("escape"))
+        {
+            see = !see;
+            UIPause.SetActive(see);
+        }
+
+    }
+
+    public void SetValume(float aim)
+    {
+        volume = aim;
+    }
+
+    public void ToMenu()
+    {
+        Destroy(music);
+        SceneManager.LoadScene("_WordGame_Menu");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("__WordGame_Scene_0");
+        DontDestroyOnLoad(music);
+    }
+
+    
+}
+
+
+
+
+
+
